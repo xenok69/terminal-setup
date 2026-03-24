37ff8bcd5a6726959c3b265cd6ee5aece91ff590
xenok1 <97119602+xenok69@users.noreply.github.com>
2026-03-24 17:01:03 +0100

    Update Microsoft.PowerShell_profile.ps1

--- bin/Microsoft.PowerShell_profile.ps1
+++ bin/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
+winget upgrade --id Microsoft.PowerShell
+
 oh-my-posh init pwsh --config "https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/uew.omp.json" --eval | Invoke-Expression
 
 clear
-
 fastfetch

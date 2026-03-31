843d09d0d29408076af8c20e47f15b424581dc02
xenok1 <y.schocher@brainnote.com>
2026-03-31 17:26:15 +0200

    New config :)

--- bin/Microsoft.PowerShell_profile.ps1
+++ bin/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
-oh-my-posh init pwsh --config "https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/uew.omp.json" --eval | Invoke-Expression
+winget upgrade --id Microsoft.PowerShell
+
+oh-my-posh init pwsh --config "https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/darkblood.omp.json" --eval | Invoke-Expression
 
 clear
-
 fastfetch

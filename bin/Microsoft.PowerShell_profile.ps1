cf25602c7dfe8e4ad7b1d37c5e9f8bd35a5a9d2c
xenok1 <y.schocher@brainnote.com>
2026-03-31 17:41:01 +0200

    Adjusted colors on oh-my-posh config

--- bin/Microsoft.PowerShell_profile.ps1
+++ bin/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 winget upgrade --id Microsoft.PowerShell
 
-oh-my-posh init pwsh --config "https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/darkblood.omp.json" --eval | Invoke-Expression
+oh-my-posh init pwsh --config "https://raw.githubusercontent.com/xenok69/terminal-setup/main/bin/oh-my-posh_custom.json" --eval | Invoke-Expression
 
 clear
 fastfetch
